40ace592df93dd862403f1496b8a5a38b1295a8a
zsq1991 <15501160754@163.com>
2017-12-17 14:22:32 +0800

    Initial commit

---
+++ p2p-manager-front/src/main/java/com/p2p/FrontApplication.java
@@ -0,0 +1,19 @@
+package com.p2p;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Administrator on 2017/12/15.
+ */
+@ComponentScan(basePackages = {"com.p2p"})
+/*配置到mapper*/
+@MapperScan("com.p2p.mapper")
+@SpringBootApplication
+public class FrontApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontApplication.class);
+    }
+}
