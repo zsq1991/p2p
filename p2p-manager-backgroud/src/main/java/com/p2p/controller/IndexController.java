eb178eb4838ef294689824907227329c58237918
zsq1991 <15501160754@163.com>
2017-12-18 08:03:46 +0800

    修改

--- p2p-manager-backgroud/src/main/java/com/p2p/controller/IndexController.java
+++ p2p-manager-backgroud/src/main/java/com/p2p/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Administrator on 2017/12/15.
  */
-
+//
 @RestController
 public class IndexController {
     @RequestMapping("/test")
